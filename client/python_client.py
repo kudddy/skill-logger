ef5aa28a90757c199cce00a5575e26627cdce600
16783869 <Khudyakov.K.Ko@sberbank.ru>
2022-04-15 13:47:34 +0300

    подправил ReadMe

--- client/python_client.py
+++ client/python_client.py
@@ -3,8 +3,8 @@ from handlers.apig_sdk import signer
 from json import dumps, loads
 
 sig = signer.Signer()
-sig.Key = "21fbb619dece43deb1c80b3280a54f6c"
-sig.Secret = "94a2d93bfb7a4cb4bfcc4bf31b1f37a1"
+sig.Key = "***"
+sig.Secret = "***"
 
 
 def send_log(log: str, bot_name: str, func_host: str):
@@ -24,12 +24,7 @@ def send_log(log: str, bot_name: str, func_host: str):
 
     data = requests.request(r.method, r.scheme + "://" + r.host + r.uri, headers=r.headers, data=r.body, verify=False)
 
-    print(data.status_code, data.reason)
-    print(data.content)
-
-    print(loads(data.content))
-
 
 send_log(log="sdfafsdaf",
          bot_name="sberauto",
-         func_host="https://smapi.pv-api.sbc.space/fn_df4f75ea_c184_4999_92ae_5f0f20ec9f2b")
+         func_host="***")
